8599c49e211bb284bda55c39496fe1d284543741
Adarsh <72383279+devAdarsh19@users.noreply.github.com>
2022-03-27 14:18:08 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,28 +3,28 @@ struct students {
 	char name[20], usn[20];
 	float m1, m2, m3, total, avg;
 };
-struct student s;
+struct student s[20];
 int main() {
 	int n, i;
 	printf("Enter number of students: ");
 	scanf(" %d", &n);
 
 	for (i = 0; i < n; i++) {
-		printf("Enter name: ");
-		scanf(" %s", s.name);
-		printf("Enter usn: ");
-		scanf(" %s", s.usn);
-		printf("Enter marks: ");
-		scanf(" %f%f%f", &s.m1, &s.m2, &s.m3);
-		s.total = s.m1 + s.m2 + s.m3;
-		s.avg = s.total / 3;
+		printf("\nEnter name: ");
+		scanf(" %s", s[i].name);
+		printf("\nEnter usn: ");
+		scanf(" %s", s[i].usn);
+		printf("\nEnter marks: ");
+		scanf(" %f%f%f", &s[i].m1, &s[i].m2, &s[i].m3);
+		s[i].total = s[i].m1 + s[i].m2 + s[i].m3;
+		s[i].avg = s[i].total / 3;
 	}
 	for (i = 0; i < n; i ++) {
-		if (s.avg > 35) {
-			printf("\n%s has scored above average", s.name);
+		if (s[i].avg > 35) {
+			printf("\n%s has scored above average", s[i].name);
 		}
 		else {
-			printf("\n%s has scored below average.", s.name);
+			printf("\n%s has scored below average.", s[i].name);
 		}
 	}
 	
